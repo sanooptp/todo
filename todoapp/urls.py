9e55349de56250cc9f7ff91e88551e7b0434755c
sanooptp <isanooptp@gmail.com>
2023-05-23 20:38:17 +0530

    dfds

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("list/", views.TodoListView.as_view(), name='list'),
-    path("listitems/", views.TodoListView.as_view(), name='list'),
+    path("listitems/", views.TodoListByoneView.as_view(), name='list'),
     path("create/", views.TodoCreateView.as_view(), name='create'),
     path("update/<int:pk>/", views.TodoUpdateView.as_view(), name='update'),
-    path("delete/<int:pk>/", views.TodoListByoneView.as_view(), name='delete'),
+    path("delete/<int:pk>/", views.TodoDeleteView.as_view(), name='delete'),
 ]
